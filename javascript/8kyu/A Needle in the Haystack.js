6a14a205a5015b9a25de7c8c645f6d4c21ea80df
checkmyprojects <notnow.plz+github@gmail.com>
2022-04-14 13:28:53 +0200

    js 8kyu A Needle in the Haystack done

--- javascript/8kyu/A Needle in the Haystack.js
+++ javascript/8kyu/A Needle in the Haystack.js
@@ -12,4 +12,12 @@
 
 // https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
 
+function findNeedle(haystack) {
+    // your code here
+    for(let i = 0; i< haystack.length; i++){
+      if(haystack[i] === "needle"){
+        return `found the needle at position ${i}`
+      }
+    }
+  }
 
