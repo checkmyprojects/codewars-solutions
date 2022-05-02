d9bc7b866483a90066ca4ba728e836230002f849
checkmyprojects <notnow.plz+github@gmail.com>
2022-05-02 10:07:52 +0200

    js 8kyu MakeUpperCase

---
+++ javascript/8kyu/MakeUpperCase.js
@@ -0,0 +1,8 @@
+// Write a function which converts the input string to uppercase.
+
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase();
+}
