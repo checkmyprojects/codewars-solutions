c97802c310c795c087db92845681264038a1c7ec
checkmyprojects <notnow.plz+github@gmail.com>
2022-05-24 08:35:11 +0200

    js 8kyu Remove exclamation marks

---
+++ javascript/8kyu/Remove exclamation marks.js
@@ -0,0 +1,7 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+function removeExclamationMarks(s) {
+    return s.split("!").join("");
+}
