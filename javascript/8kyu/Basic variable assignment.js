1169d4b777c7017342f35965f0b6af633bdf034f
checkmyprojects <notnow.plz+github@gmail.com>
2022-06-27 08:51:32 +0200

    js 8kyu Basic variable assignment

---
+++ javascript/8kyu/Basic variable assignment.js
@@ -0,0 +1,7 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// https://www.codewars.com/kata/50ee6b0bdeab583673000025
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
