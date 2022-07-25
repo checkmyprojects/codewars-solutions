127ec937bfbb0ce4ed0d42ac877111bf6e530330
checkmyprojects <notnow.plz+github@gmail.com>
2022-07-25 09:21:47 +0200

    js 8kyu Bin to Decimal

---
+++ javascript/8kyu/Bin to Decimal.js
@@ -0,0 +1,7 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
+
+function binToDec(bin){
+    return parseInt(bin,2);
+}
