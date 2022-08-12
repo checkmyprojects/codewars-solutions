cd8f7e9adfd56acb7b21901527936648557db0c5
checkmyprojects <notnow.plz+github@gmail.com>
2022-08-12 11:17:17 +0200

    js 8kyu Grasshopper - Function syntax debugging

---
+++ javascript/8kyu/Grasshopper - Function syntax debugging.js
@@ -0,0 +1,9 @@
+// Grasshopper - Function syntax debugging
+
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// https://www.codewars.com/kata/56dae9dc54c0acd29d00109a
+
+function main (verb, noun){
+    return verb + noun
+}
