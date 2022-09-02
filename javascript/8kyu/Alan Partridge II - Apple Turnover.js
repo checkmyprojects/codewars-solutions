5c38b96653d4357e146573aa74dd1183b4c2875d
checkmyprojects <notnow.plz+github@gmail.com>
2022-09-02 07:52:06 +0200

    js 8kyu Alan Partridge II - Apple Turnover

---
+++ javascript/8kyu/Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d
+
+function apple(x){
+    if (Number(x)*Number(x)>1000){
+        return 'It\'s hotter than the sun!!';
+        }
+    
+    return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+}
