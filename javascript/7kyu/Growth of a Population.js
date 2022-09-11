0fbde902619aac6bbe676e07ed99e449a28e0346
checkmyprojects <notnow.plz+github@gmail.com>
2022-09-11 10:21:54 +0200

    js 7kyu Growth of a Population

--- javascript/7kyu/Growth of a Population.js
+++ javascript/7kyu/Growth of a Population.js
@@ -4,8 +4,8 @@ function nbYear(p0, percent, aug, p) {
   let year = 0;
   let population = p0;
   while (population < p) {
-			population = Math.floor((1 + percent/100)* population) + aug;
-			year++;
+		population = Math.floor((1 + percent/100)* population) + aug;
+		year++;
   }
   return year;
 }
