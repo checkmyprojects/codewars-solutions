489c71d8919283ec1ed0a792028a944eb7cceb91
checkmyprojects <notnow.plz+github@gmail.com>
2022-09-18 18:48:04 +0200

    java Multiply

---
+++ java/8kyu/Multiply.java
@@ -0,0 +1,9 @@
+// This code does not execute properly. Try to figure out why.
+
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+public class Multiply {
+    static double multiply(double a, double b) {
+      return a * b;
+    }
+}
