bac859280ae87516b2c938481207a8493ebf7880
checkmyprojects <notnow.plz+github@gmail.com>
2022-09-19 09:24:01 +0200

    java 8kyu Convert a Number to a String!

---
+++ java/8kyu/Convert a Number to a String.java
@@ -0,0 +1,16 @@
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+class Kata {
+  public static String numberToString(int num) {
+    return num + "";
+  }
+}
