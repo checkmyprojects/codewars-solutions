259b827b2cf6e4a80eaed4f2c08da2c20baa5e20
checkmyprojects <notnow.plz+github@gmail.com>
2022-09-20 08:49:52 +0200

    java 8kyu Remove String Spaces

---
+++ java/8kyu/Remove String Spaces.java
@@ -0,0 +1,9 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+public class Kata {
+    public static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
