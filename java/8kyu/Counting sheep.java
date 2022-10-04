062ae1ffb974de6afa8348173a38f651e4d6f886
checkmyprojects <notnow.plz+github@gmail.com>
2022-10-04 07:26:58 +0200

    java 8kyu Counting sheep

---
+++ java/8kyu/Counting sheep.java
@@ -0,0 +1,31 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+// https://www.codewars.com/kata/54edbc7200b811e956000556
+
+public class Counter {
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    int sheeps = 0;
+    for(int i=0; i<arrayOfSheeps.length; i++){
+      if(arrayOfSheeps[i] == null){
+        continue;
+      }
+      if(arrayOfSheeps[i] == true){
+        sheeps++;
+      }
+    }
+    return sheeps;
+  }
+}
