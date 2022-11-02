f60220db88b0bf1456c45a84feaa72cf4c663b4f
checkmyprojects <notnow.plz+github@gmail.com>
2022-11-02 07:00:29 +0100

    java 8yu Convert a string to an array

---
+++ java/8kyu/Convert a string to an array.java
@@ -0,0 +1,16 @@
+// Write a function to split a string and convert it into an array of words.
+// Examples (Input ==> Output):
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+public class Solution {
+
+    public static String[] stringToArray(String s) {
+      return s.split(" ");
+    }
+
+}
