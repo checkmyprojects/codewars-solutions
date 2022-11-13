6c341a257724f2eadbbdebd543b918964f55ee6b
checkmyprojects <notnow.plz+github@gmail.com>
2022-11-13 10:30:59 +0100

    java 8kyu Remove exclamation marks

---
+++ java/8kyu/Remove exclamation marks.java
@@ -0,0 +1,9 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+class Solution {
+    static String removeExclamationMarks(String s) {
+        return s.replaceAll("!", "");
+    }
+}
