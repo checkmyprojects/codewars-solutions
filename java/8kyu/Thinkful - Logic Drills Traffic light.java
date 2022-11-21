9303a132d33a0c3f9ac3361466123df1a9a9e19e
checkmyprojects <notnow.plz+github@gmail.com>
2022-11-21 06:58:52 +0100

    java 8kyu Thinkful - Logic Drills: Traffic light

---
+++ java/8kyu/Thinkful - Logic Drills Traffic light.java
@@ -0,0 +1,21 @@
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// For example, when the input is green, output should be yellow.
+
+// https://www.codewars.com/kata/58649884a1659ed6cb000072
+
+public class TrafficLights {
+
+  public static String updateLight(String current) {
+  
+    if(current.equals("green")){
+      return "yellow";
+    }else if(current.equals("yellow")){
+      return "red";
+    }else{
+      return "green";
+    }
+  }
+}
