51fda2ab0b78e59fa0ce1d894fa453a530a9066f
checkmyprojects <notnow.plz+github@gmail.com>
2022-11-22 06:58:09 +0100

    java 8kyu Count Odd Numbers below n

---
+++ java/8kyu/Count Odd Numbers below n.java
@@ -0,0 +1,9 @@
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// https://www.codewars.com/kata/59342039eb450e39970000a6
+
+public class Kata {
+  public static int oddCount(int n){
+    return n/2;
+  }
+}
