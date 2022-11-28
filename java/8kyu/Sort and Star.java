d29b44783df9531fbfd90a437f53fe92be99c146
checkmyprojects <notnow.plz+github@gmail.com>
2022-11-28 06:55:45 +0100

    java 8kyu Sort and Star

---
+++ java/8kyu/Sort and Star.java
@@ -0,0 +1,14 @@
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+// https://www.codewars.com/kata/57cfdf34902f6ba3d300001e
+
+import static java.util.Arrays.stream;
+public class SortAndStar {
+  public static String twoSort(String[] s) {
+    return String.join("***", stream(s).sorted().findFirst().orElse("").split(""));
+  }
+}
