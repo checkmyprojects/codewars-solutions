8c9b731fb05d7583f2e3420a24e2175c4a51d0f3
checkmyprojects <notnow.plz+github@gmail.com>
2022-12-11 19:06:21 +0100

    java 8kyu Grasshopper - Messi goals function

---
+++ java/8kyu/Grasshopper - Messi goals function.java
@@ -0,0 +1,17 @@
+// Messi goals function
+
+// Messi is a soccer player with goals in three leagues:
+
+//     LaLiga
+//     Copa del Rey
+//     Champions
+
+// Complete the function to return his total number of goals in all three leagues.
+
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+    }
+}
