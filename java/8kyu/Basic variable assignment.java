4c371f34d8d09229590e425d95c2d095cfee5c08
checkmyprojects <notnow.plz+github@gmail.com>
2022-12-20 07:04:08 +0100

    java 8kyu Basic variable assignment

---
+++ java/8kyu/Basic variable assignment.java
@@ -0,0 +1,9 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// https://www.codewars.com/kata/50ee6b0bdeab583673000025
+
+public class Kata {
+    public static String a = "code";
+    public static String b = "wa.rs";
+    public static String name = a + b;
+}
