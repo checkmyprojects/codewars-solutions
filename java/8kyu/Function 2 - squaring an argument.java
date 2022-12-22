3a78858578f78f73c760318fc16ee28bde6100e8
checkmyprojects <notnow.plz+github@gmail.com>
2022-12-22 07:01:53 +0100

    java 8kyu Function 2 - squaring an argument

---
+++ java/8kyu/Function 2 - squaring an argument.java
@@ -0,0 +1,9 @@
+// Now you have to write a function that takes an argument and returns the square of it
+
+// https://www.codewars.com/kata/523b623152af8a30c6000027
+
+public class Kata{
+  public static int square(int n){
+    return n*n;
+  }
+ }
