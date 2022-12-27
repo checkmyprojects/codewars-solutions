c2d05820ac1d2c93f13f62e756e5c64127bfe34b
checkmyprojects <notnow.plz+github@gmail.com>
2022-12-27 07:15:11 +0100

    java 8kyu Grasshopper - Basic Function Fixer

---
+++ java/8kyu/Grasshopper - Basic Function Fixer.java
@@ -0,0 +1,10 @@
+// Fix the function
+
+// https://www.codewars.com/kata/56200d610758762fb0000002
+
+public class Solution {
+  public static int addFive(int num) {
+    int total = num + 5;
+    return total;
+  }
+}
