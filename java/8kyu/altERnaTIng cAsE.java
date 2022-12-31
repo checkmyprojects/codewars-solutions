91820a8a2317349fb2cb9c5ba560068d58c3424d
checkmyprojects <notnow.plz+github@gmail.com>
2022-12-31 10:36:09 +0100

    java 8kyu altERnaTIng cAsE <=> ALTerNAtiNG CaSe

---
+++ java/8kyu/altERnaTIng cAsE.java
@@ -0,0 +1,17 @@
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// https://www.codewars.com/kata/56efc695740d30f963000557
+
+public class StringUtils {
+    public static String toAlternativeString(String string) {
+        String result = "";
+        for (char c : string.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                result += Character.toLowerCase(c);
+            } else {
+                result += Character.toUpperCase(c);
+            }
+        }
+        return result;
+    }
+}
