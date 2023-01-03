a93f9ee3a29a68cb81e2ef007e5d13a13eb897d0
checkmyprojects <notnow.plz+github@gmail.com>
2023-01-03 09:40:39 +0100

    java 8kyu get character from ASCII Value

---
+++ java/8kyu/get character from ASCII Value.java
@@ -0,0 +1,9 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c
+
+public class Ascii {
+  public static char getChar(int c) {
+    return (char) c;
+  }
+}
