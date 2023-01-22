3a4adf53ace9ab39375d2adb2b1c59667ff73d44
checkmyprojects <notnow.plz+github@gmail.com>
2023-01-22 10:05:32 +0100

    java 8kyu https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e

---
+++ java/8kyu/Sum of differences in array.java
@@ -0,0 +1,12 @@
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e
+
+import java.util.Arrays;
+
+public class ZywOo {
+  public static int sumOfDifferences(int[] arr) {
+    Arrays.sort(arr);
+		return arr.length <= 1 ? 0 : arr[arr.length-1] - arr[0];
+  }
+}
