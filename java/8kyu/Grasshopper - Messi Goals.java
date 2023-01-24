adf9d8a5a4c59c7a2821db9dbd876f2f0947ea32
checkmyprojects <notnow.plz+github@gmail.com>
2023-01-24 06:44:14 +0100

    java 8kyu Grasshopper - Messi Goals

---
+++ java/8kyu/Grasshopper - Messi Goals.java
@@ -0,0 +1,12 @@
+// Messi's Goal Total
+
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+
+class Goals {
+  static int laLigaGoals = 43;
+  static int championsLeagueGoals = 10;
+  static int copaDelReyGoals = 5;
+  static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+}
