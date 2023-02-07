64009d5f346218622295e2ab8b76cca319091cc1
checkmyprojects <notnow.plz+github@gmail.com>
2023-02-07 06:46:36 +0100

    java 8kyu A Strange Trip to the Market

---
+++ java/8kyu/A Strange Trip to the Market.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55ccdf1512938ce3ac000056
+
+public class Nessie {
+    public static boolean isLockNessMonster(String s){
+        return s.contains("tree fiddy") || s.contains("three fifty") || s.contains("3.50");
+    }
+}
