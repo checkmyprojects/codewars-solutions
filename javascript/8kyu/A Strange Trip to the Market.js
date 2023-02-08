d3a3899a7b143e1ba69d868c5ea6b177577fe3a8
checkmyprojects <notnow.plz+github@gmail.com>
2023-02-08 06:55:48 +0100

    js 8kyu A Strange Trip to the Market

---
+++ javascript/8kyu/A Strange Trip to the Market.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/55ccdf1512938ce3ac000056
+
+function isLockNessMonster(s) {
+    return s.includes("tree fiddy") || s.includes("three fifty") || s.includes("3.50");
+  }
