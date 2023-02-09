9f014d27338cfbb8060b7f509f103a58c99fd61f
checkmyprojects <notnow.plz+github@gmail.com>
2023-02-09 06:53:30 +0100

    js 8kyu Kata Example Twist

---
+++ javascript/8kyu/Kata Example Twist.js
@@ -0,0 +1,10 @@
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+// https://www.codewars.com/kata/525c1a07bb6dda6944000031
+
+var websites = []
+for(let i=0; i<1000; i++){
+  websites.push("codewars");
+}
