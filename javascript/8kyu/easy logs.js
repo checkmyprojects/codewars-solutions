82d58fcc128c60573c05a02027a5ff4ae357880d
checkmyprojects <notnow.plz+github@gmail.com>
2023-03-21 08:03:33 +0100

    js 8kyu easy logs

---
+++ javascript/8kyu/easy logs.js
@@ -0,0 +1,7 @@
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+// https://www.codewars.com/kata/5b68c7029756802aa2000176
+
+function logs(x , a, b){
+    return Math.log(a * b) / Math.log(x);
+  }
