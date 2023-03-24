a40a6459bdd350b367718d2d2d9eefadb5242fcf
checkmyprojects <notnow.plz+github@gmail.com>
2023-03-24 06:56:04 +0100

    js 8kyu Training JS #1: create your first JS function and print Hello World!

---
+++ javascript/8kyu/Training JS 1.js
@@ -0,0 +1,8 @@
+// In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
+
+function helloWorld(){
+    let str = "Hello World!";
+    console.log(str);
+  }
