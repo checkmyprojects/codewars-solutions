92e549c4aeda6bed4204449341434d37ec49c03b
checkmyprojects <notnow.plz+github@gmail.com>
2023-03-25 09:16:32 +0100

    java 8kyu Exclusive or xor Logical Operator

---
+++ java/8kyu/Exclusive or xor Logical Operator.java
@@ -0,0 +1,10 @@
+//In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata).
+// The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise.
+
+// https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c
+
+public class XOR {
+    public static boolean xor(boolean a, boolean b) {
+        return a ^ b;
+    }
+}
