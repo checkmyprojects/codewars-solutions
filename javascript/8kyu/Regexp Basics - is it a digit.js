d2a255695643062baaa3d9994fa32be16379787d
checkmyprojects <notnow.plz+github@gmail.com>
2023-03-31 17:04:37 +0200

    js 8kyu Regexp Basics - is it a digit?

---
+++ javascript/8kyu/Regexp Basics - is it a digit.js
@@ -0,0 +1,7 @@
+// Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+
+// https://www.codewars.com/kata/567bf4f7ee34510f69000032
+
+String.prototype.digit = function() {
+    return /^\d$/.test(this);
+};
