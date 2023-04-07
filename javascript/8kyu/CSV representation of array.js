d9a0ba2211cf333b9f98b34a0e9d1e8a03ae4122
checkmyprojects <notnow.plz+github@gmail.com>
2023-04-07 09:52:09 +0200

    js 8kyu CSV representation of array

---
+++ javascript/8kyu/CSV representation of array.js
@@ -0,0 +1,7 @@
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036
+
+function toCsvText(array) {
+    return array.join('\n');
+ }
