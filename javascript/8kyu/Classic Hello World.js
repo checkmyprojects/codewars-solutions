b84bc99d9c2e419c968d5e3bf0ca75af0dc4e713
checkmyprojects <notnow.plz+github@gmail.com>
2023-04-23 10:32:20 +0200

    js 8kyu Classic Hello World

---
+++ javascript/8kyu/Classic Hello World.js
@@ -0,0 +1,9 @@
+// You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+// https://www.codewars.com/kata/57036f007fd72e3b77000023
+
+class Solution{
+    static main(){
+        console.log('Hello World!');
+    }
+}
