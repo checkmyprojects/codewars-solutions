aaddbca54b0f4ede1a4b8426bcaf80a7090ea4cf
checkmyprojects <notnow.plz+github@gmail.com>
2023-04-27 06:48:36 +0200

    java 8kyu Wilson primes

---
+++ java/8kyu/Wilson primes.java
@@ -0,0 +1,9 @@
+// Your task is to create a function that returns true if the given number is a Wilson prime.
+
+// https://www.codewars.com/kata/55dc4520094bbaf50e0000cb
+
+public class WilsonPrime {
+	public static boolean am_i_wilson(double n) {
+    return n == 5 || n == 13 || n == 563;
+  }
+}
