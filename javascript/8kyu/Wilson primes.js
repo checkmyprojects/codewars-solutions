a7f3b745c003a261596b5ddb9ece06e5003916a5
checkmyprojects <notnow.plz+github@gmail.com>
2023-04-28 06:46:12 +0200

    js 8kyu Wilson primes

---
+++ javascript/8kyu/Wilson primes.js
@@ -0,0 +1,7 @@
+// Your task is to create a function that returns true if the given number is a Wilson prime.
+
+// https://www.codewars.com/kata/55dc4520094bbaf50e0000cb
+
+function amIWilson(p) {
+    return p == 5 || p == 13 || p == 563;
+  }
