5745ab0d0e75e12c71875b11ba5f70f828a27e12
checkmyprojects <notnow.plz+github@gmail.com>
2023-04-29 11:32:53 +0200

    js 8kyu Grasshopper - Combine strings

---
+++ javascript/8kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,7 @@
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// https://www.codewars.com/kata/55f73f66d160f1f1db000059
+
+function combineNames(first, last){
+    return first + ' ' + last;
+  }
