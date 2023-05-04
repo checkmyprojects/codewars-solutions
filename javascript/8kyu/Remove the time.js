8f668f8409304b25fbef21805ee8188a913b089d
checkmyprojects <notnow.plz+github@gmail.com>
2023-05-04 06:50:44 +0200

    js 8kyu Remove the time

---
+++ javascript/8kyu/Remove the time.js
@@ -0,0 +1,7 @@
+// Write a function that takes the website date/time in its original string format and returns the shortened format.
+
+// https://www.codewars.com/kata/56b0ff16d4aa33e5bb00008e
+
+function shortenToDate(longDate) {
+    return(longDate.split(",")[0]);
+}
