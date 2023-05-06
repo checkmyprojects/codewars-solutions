4ebe0000062d1958c122454e09cca40aec8d8a7d
checkmyprojects <notnow.plz+github@gmail.com>
2023-05-06 08:58:27 +0200

    js 8kyu Grasshopper - Variable Assignment Debug

---
+++ javascript/8kyu/Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,8 @@
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+// https://www.codewars.com/kata/5612e743cab69fec6d000077
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
