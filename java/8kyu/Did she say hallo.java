1c3d3bcb7dbc65a179ae45b91ad1d78734d908af
checkmyprojects <notnow.plz+github@gmail.com>
2023-05-10 06:49:33 +0200

    java 8kyu Did she say hallo

---
+++ java/8kyu/Did she say hallo.java
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/56a4addbfd4a55694100001f
+
+public class Kata{
+  public static boolean validateHello(String greetings){
+    return greetings.matches("(?i).*(hello|ciao|salut|hallo|hola|ahoj|czesc).*");
+  }
+  
+}
