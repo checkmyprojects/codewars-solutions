880294d459179a6d68509c42edceb0f67b5a7aea
checkmyprojects <notnow.plz+github@gmail.com>
2023-05-17 06:48:05 +0200

    java 8kyu How do I compare numbers

---
+++ java/8kyu/How do I compare numbers.java
@@ -0,0 +1,18 @@
+// What could be easier than comparing integer numbers? However, the given piece of code doesn't recognize some of the special numbers for a reason to be found. Your task is to find the bug and eliminate it.
+
+// https://www.codewars.com/kata/55d8618adfda93c89600012e
+
+public class HowDoICompare {
+    public static String whatIs(Integer x) {
+        for (Object[] p : specialNumbers) {
+            if (p[0].equals(x))
+                return (String)p[1];
+        }
+        return "nothing";
+    }
+    
+    static final Object[][] specialNumbers = {
+        {42, "everything"},
+        {42 * 42, "everything squared"},
+    };
+}
