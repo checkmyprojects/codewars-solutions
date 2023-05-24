ba74351cd493248ab34db27145982699453d8b7f
checkmyprojects <notnow.plz+github@gmail.com>
2023-05-24 06:54:32 +0200

    java 8kyu A plus B

---
+++ java/8kyu/A plus B.java
@@ -0,0 +1,9 @@
+// Vasya Pupkin just started learning Java and C#. At first, he decided to write simple program that was supposed to sum up two small numbers (numbers and their sum fit in a byte), but it didn't work. Vasya was too sad to find out what is wrong. Help him to correct the mistake.
+
+// https://www.codewars.com/kata/5512a0b0509063e57d0003f5
+
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        return (byte) (a + b);
+    }
+}
