21f262832d45c9fdd34bfa29d6fa7ad8b51b9fd6
checkmyprojects <notnow.plz+github@gmail.com>
2023-05-28 09:32:52 +0200

    js 8kyu Be Concise IV - Index of an element in an array

---
+++ javascript/8kyu/Be Concise IV - Index of an element in an array.js
@@ -0,0 +1,5 @@
+// Provided is a function Kata which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 161) You may assume that all array elements are unique.
+
+// https://www.codewars.com/kata/5703c093022cd1aae90012c9
+
+function find(a, e) {return a.includes(e)?a.indexOf(e):"Not found";}
