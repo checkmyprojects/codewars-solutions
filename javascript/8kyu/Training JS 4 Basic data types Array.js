333623103de4f188a8560e75a22d3227479cb842
checkmyprojects <notnow.plz+github@gmail.com>
2023-06-05 06:51:01 +0200

    js 8kyu Training JS 4 Basic data types Array

---
+++ javascript/8kyu/Training JS 4 Basic data types Array.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/571effabb625ed9b0600107a
+
+const getLength = array => array.length;
+
+const getFirst = array => array[0];
+
+const getLast = array => array[array.length - 1];
+
+const pushElement = array => array.concat(0);
+
+const popElement = array => array.splice(0, array.length - 1);
