60138675b9b5d45ef507d4bd3b320757bdbdbff9
checkmyprojects <notnow.plz+github@gmail.com>
2023-06-23 07:09:54 +0200

    js 8kyu Incorrect division method

---
+++ javascript/8kyu/Incorrect division method.js
@@ -0,0 +1,5 @@
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+// https://www.codewars.com/kata/54d1c59aba326343c80000e7
+
+const solve = (x, y) => x / y
