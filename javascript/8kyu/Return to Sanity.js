48d44a6201ec91b393e1ae916ff3ade195b460f0
checkmyprojects <notnow.plz+github@gmail.com>
2023-06-28 06:51:57 +0200

    js 8kyu Return to Sanity

---
+++ javascript/8kyu/Return to Sanity.js
@@ -0,0 +1,8 @@
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+// https://www.codewars.com/kata/514a7ac1a33775cbb500001e
+
+function mystery() {
+    var result = {'sanity': 'Hello'};
+    return result;
+  }
