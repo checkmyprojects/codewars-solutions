ed32bc02f04258e3b81348afcbc82af380475bd3
checkmyprojects <notnow.plz+github@gmail.com>
2023-07-04 06:44:38 +0200

    js 8kyu Grasshopper - Order of operations

---
+++ javascript/8kyu/Grasshopper - Order of operations.js
@@ -0,0 +1,7 @@
+// You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
+
+// https://www.codewars.com/kata/560ecf0cb040de130e00007d
+
+function orderOperations () {
+    return (2 + 2) * (2 + 2) * 2;
+  }
