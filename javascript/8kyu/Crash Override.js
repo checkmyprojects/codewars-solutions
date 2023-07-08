36496d6a02480e1870224226edd5f1d404a5f6b5
checkmyprojects <notnow.plz+github@gmail.com>
2023-07-08 08:46:54 +0200

    js 8kyu Crash Override

---
+++ javascript/8kyu/Crash Override.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/578c1e2edaa01a9a02000b7f
+
+function aliasGen(first,last){
+    let auxFirst = first[0].toUpperCase( )
+    let auxLast = last[0].toUpperCase( )
+  
+    if(firstName[auxFirst] && surname[auxLast]) {
+      return firstName[auxFirst] + ' ' + surname[auxLast]
+    } else {
+      return 'Your name must start with a letter from A - Z.'
+    }
+  }
