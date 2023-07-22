9b29e2679210eaa667e5157c02337b160da2c3ef
checkmyprojects <notnow.plz+github@gmail.com>
2023-07-22 08:46:22 +0200

    js 8kyu For UFC Fans

---
+++ javascript/8kyu/For UFC Fans.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/582dafb611d576b745000b74
+
+var quote = function(fighter) {
+    return fighter.toLowerCase() === "george saint pierre" ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
