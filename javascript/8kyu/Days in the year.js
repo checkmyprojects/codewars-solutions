f05defa322ed7523145b5a99f5482aae7e8f2584
checkmyprojects <notnow.plz+github@gmail.com>
2023-07-27 06:42:23 +0200

    js 8kyu Days in the year

---
+++ javascript/8kyu/Days in the year.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56d6c333c9ae3fc32800070f
+
+function yearDays(year) {
+    let nDays =  year % 4 ? 365 :
+                 year % 100 ? 366:
+                 year % 400 ? 365 : 366;
+    
+    return `${year} has ${nDays} days`;
+  }
