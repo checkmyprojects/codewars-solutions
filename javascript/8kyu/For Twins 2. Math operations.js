3cc1fd2fe4bc3f76039e7d7a1c5a40ef9fc03176
checkmyprojects <notnow.plz+github@gmail.com>
2023-09-03 09:35:40 +0200

    js 8kyu For Twins: 2. Math operations

---
+++ javascript/8kyu/For Twins 2. Math operations.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/59c287b16bddd291c700009a
+
+function iceBrickVolume(radius, bottleLength, rimLength) {
+    return (bottleLength - rimLength) * radius * 2 * radius;
+}
