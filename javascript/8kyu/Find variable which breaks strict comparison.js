18d3dc879e10b69e5423c63b7d58d4dbcb767f11
checkmyprojects <notnow.plz+github@gmail.com>
2023-09-22 06:55:25 +0200

    js 8kyu Find variable which breaks strict comparison

---
+++ javascript/8kyu/Find variable which breaks strict comparison.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/560f8d41cf6e1fe5c900002e
+
+// Write a function which will return value for which strict comparison will give false!
+
+function findStrangeValue() {
+    return NaN;
+}
