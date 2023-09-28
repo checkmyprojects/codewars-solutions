2bebc79046ee0a0bb43bcaf6908b2d919b23f826
checkmyprojects <notnow.plz+github@gmail.com>
2023-09-28 06:43:13 +0200

    js 8kyu Shifty Closures

---
+++ javascript/8kyu/Shifty Closures.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/514aa0dc21607ae236000017
+
+var name;
+function greet_abe() {
+  name = 'Abe';
+  return "Hello, " + name + '!';
+};
+
+
+function greet_ben() {
+  name = 'Ben'; 
+  return "Hello, " + name + '!';
+};
