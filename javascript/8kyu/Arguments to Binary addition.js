fdc69c48b5df4ef8cd9558a595ab808dd1b8284b
checkmyprojects <notnow.plz+github@gmail.com>
2023-09-29 06:55:24 +0200

    js 8kyu Arguments to Binary addition

---
+++ javascript/8kyu/Arguments to Binary addition.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/57642a90dee2da8dd3000161
+
+// Given an array add all the number elements and return the binary equivalent of that sum; to make the code bullet proof anything else than a number should be addeded as 0 since it can't be addeded.
+
+// If your language can handle float binaries assume the array won't contain float or doubles.
+
+// arr2bin([1,2]) == '11'
+// arr2bin([1,2,'a']) == '11'
+// arr2bin([]) == '0'
+
+// NOTE: NaN is a number too in javascript for decimal, binary and n-ary base
+
+function arr2bin(arr){
+    return arr.reduce((x,y)=>x+(typeof y=="number"?y:0),0).toString(2);
+  }
