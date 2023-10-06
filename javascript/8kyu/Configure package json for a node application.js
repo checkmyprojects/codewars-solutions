8b43d5ce2e61cbf519c04e693857fc64447d3edc
checkmyprojects <notnow.plz+github@gmail.com>
2023-10-06 06:47:01 +0200

    js 8kyu Configure package json for a node application

---
+++ javascript/8kyu/Configure package json for a node application.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/5692a582cc22387ccf000017
+
+const configuration = {
+    "name": "pack-name",
+    "version": "1.0.0",
+    "description": "description",
+    "main": "index.js",
+    "scripts": {
+      "start": "node index.js"
+    },
+    "keywords": ["keyword1", "keyword2"],
+    "bugs": {
+      "url": "https://github.com/issues",
+      "email": "your.email@example.com"
+    },
+    "license": "MIT",
+    "author": {
+      "name": "Name",
+      "email": "email@example.com",
+      "url": "https://yourwebsite.com"
+    },
+    "repository": {
+      "type": "git",
+      "url": "https://github.com/.git"
+    },
+    "private": false,
+    "engines": {
+      "node": ">=10.0.0"
+    },
+    "dependencies": {
+      "babel-core": "^6.0.0",
+      "babel-polyfill": "^6.0.0",
+      "babel-preset-es2015": "^6.0.0",
+      "babel-preset-stage-0": "^6.0.0"
+    },
+    "devDependencies": {
+      "eslint": "^7.0.0",
+      "mocha": "^8.0.0"
+    }
+  };
