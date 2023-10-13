d1730e5249320de40c0f5330ed3b244d34a8be35
checkmyprojects <notnow.plz+github@gmail.com>
2023-10-13 06:45:11 +0200

    js 8kyu SpeedCode 3 x Fun with ES6 Classes 5 - Dogs and Classes

---
+++ javascript/8kyu/SpeedCode 3 x Fun with ES6 Classes 5 - Dogs and Classes.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56ff9b53140fcca90b000530
+
+class Labrador extends Dog {
+    constructor(name, age, gender, master) {
+        super(name, age, gender, "Labrador", "Large", master, true);
+    }
+}
