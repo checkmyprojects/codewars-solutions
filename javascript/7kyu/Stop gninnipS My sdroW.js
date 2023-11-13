4c2d71e176363f03e81a813a63d95df2b7a14023
checkmyprojects <notnow.plz+github@gmail.com>
2023-11-13 06:46:12 +0100

    js 8kyu Stop gninnipS My sdroW

---
+++ javascript/7kyu/Stop gninnipS My sdroW.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5264d2b162488dc400000001
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+function spinWords(words){
+    return words.split(' ').map(function (word) {
+      return (word.length > 4) ? word.split('').reverse().join('') : word;
+    }).join(' ');
+  }
