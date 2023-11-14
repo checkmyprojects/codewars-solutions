7e28de8628b704e1fc8f9a37f63fddfbaaff1557
checkmyprojects <notnow.plz+github@gmail.com>
2023-11-14 06:43:53 +0100

    js 7kyu Remove anchor from URL

---
+++ javascript/7kyu/Remove anchor from URL.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/51f2b4448cadf20ed0000386
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed. 
+
+function removeUrlAnchor(url){
+    return url.split('#')[0];
+}
