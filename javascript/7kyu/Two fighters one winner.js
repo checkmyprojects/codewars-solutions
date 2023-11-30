25bb0e6582fc1c0cb077314d936f9e26fd1bed78
checkmyprojects <notnow.plz+github@gmail.com>
2023-11-30 06:46:28 +0100

    js 7kyu Two fighters one winner

---
+++ javascript/7kyu/Two fighters one winner.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/577bd8d4ae2807c64b00045b
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+    var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+    if(fac1 < fac2) {
+      return fighter2.name;
+    } else if(fac2 < fac1) {
+      return fighter1.name;
+    } else {
+      return firstAttacker;
+    }
+  }
