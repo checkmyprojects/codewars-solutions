52b60b5db39e62ca88a3dbeb5f8902040dface91
checkmyprojects <notnow.plz+github@gmail.com>
2024-01-07 09:17:25 +0100

    js 7kyu Parts of a list

---
+++ javascript/7kyu/Parts of a list.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/56f3a1e899b386da78000732
+
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+//     Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+//     Each part will be in a string
+//     Elements of a pair must be in the same order as in the original array.
+
+function partList(arr) {
+    var newArray;
+    var returnArray=[];
+    for( var i=1; i<arr.length; i++){
+      newArray = [];
+      newArray.push(arr.slice(0,i).join(" "));
+      newArray.push(arr.slice(i).join(" "));
+      returnArray.push(newArray);
+    }
+    
+    return returnArray;
+}
