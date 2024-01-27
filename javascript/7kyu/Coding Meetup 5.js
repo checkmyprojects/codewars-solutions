28dfed83f4862ac0716cd3fac8a91693d75ca5cb
checkmyprojects <notnow.plz+github@gmail.com>
2024-01-27 09:08:19 +0100

    js 7kyu Coding Meetup 5

---
+++ javascript/7kyu/Coding Meetup 5.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5828713ed04efde70e000346
+
+// You will be given an array of objects (associative arrays in PHP, table in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an object (associative array in PHP, table in COBOL) which includes the count of each coding language represented at the meetup.
+
+function countLanguages(list) {
+    var count = {};
+    list.forEach(x => count[x.language] = (count[x.language] || 0) + 1);
+    return count;
+}
