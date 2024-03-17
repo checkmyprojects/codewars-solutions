54dbc0a15a95b410ebd46ad1c1d5f622f90e6444
checkmyprojects <notnow.plz+github@gmail.com>
2024-03-17 10:42:11 +0100

    js 7kyu Minimize Sum Of Array Array Series 1

---
+++ javascript/7kyu/Minimize Sum Of Array Array Series 1.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5a523566b3bfa84c2e00010b
+
+// Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+
+function minSum(arr) {
+    arr.sort((x,y)=>x-y)
+    s=0
+    while(arr.length)s+=arr.pop()*arr.shift()
+    return s
+}
