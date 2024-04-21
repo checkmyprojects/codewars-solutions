d0fb3c91302e6aac1989888478859afb5c5528cf
checkmyprojects <notnow.plz+github@gmail.com>
2024-04-21 10:48:46 +0200

    js 7kyu Tidy Number Special Numbers Series 9

--- javascript/7kyu/Tidy Number Special Numbers Series 9.js
+++ javascript/7kyu/Tidy Number Special Numbers Series 9.js
@@ -2,6 +2,7 @@
 
 // A Tidy number is a number whose digits are in non-decreasing order.
 // Given a number, Find if it is Tidy or not . 
+
 function tidyNumber(n) {
   return n.toString().split('').map(Number).sort((a, b) => a - b).join('') == n.toString();
 }
