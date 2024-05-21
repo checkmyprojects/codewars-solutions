c46c3ab258ad374f1441028f66d8cb25a9d98f8f
checkmyprojects <notnow.plz+github@gmail.com>
2024-05-21 06:46:13 +0200

    js 7kyu Digitize

---
+++ javascript/7kyu/Digitize.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5417423f9e2e6c2f040002ae
+
+// Given a non-negative integer, return an array / a list of the individual digits in order.
+
+// Examples:
+// 123 => [1,2,3]
+// 1 => [1]
+// 8675309 => [8,6,7,5,3,0,9]
+
+function digitize(n) {
+    return String(n).split('').map(Number);
+}
