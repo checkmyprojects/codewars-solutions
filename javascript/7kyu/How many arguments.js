5e3634a8625945acbdd2031141f9ae2e21f1acef
checkmyprojects <notnow.plz+github@gmail.com>
2024-06-09 12:59:09 +0200

    js 7kyu How many arguments

---
+++ javascript/7kyu/How many arguments.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5c44b0b200ce187106452139
+
+// Write a function that returns the number of arguments it received.
+// Examples:
+
+function args_count() {
+    return arguments.length;
+  }
