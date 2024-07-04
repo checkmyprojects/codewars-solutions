697231ec671b89f0f879bc126e78c54d0753e972
checkmyprojects <notnow.plz+github@gmail.com>
2024-07-04 06:46:24 +0200

    js 7kyu Covfefe

---
+++ javascript/7kyu/Covfefe.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/592fd8f752ee71ac7e00008a
+
+// Your are given a string. You must replace any occurence of the sequence coverage by covfefe, however, if you don't find the word coverage in the string, you must add covfefe at the end of the string with a leading space.
+
+// For the languages where the string is mutable (such as ruby), don't modify the given string, otherwise this will break the test cases.
+
+function covfefe(str) {
+    let result = str.replace(/coverage/g, "covfefe")
+    return result === str ? str + " covfefe" : result
+}
