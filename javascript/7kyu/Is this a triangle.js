a92bcae1825c88c31cfe4a4fc8c058af7ea3834f
checkmyprojects <notnow.plz+github@gmail.com>
2024-07-13 11:05:59 +0200

    js 7kyu Is this a triangle

---
+++ javascript/7kyu/Is this a triangle.js
@@ -0,0 +1,10 @@
+// Is this a triangle?
+
+// https://www.codewars.com/kata/56606694ec01347ce800001b
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+function isTriangle(a,b,c)
+{
+   return a + b > c && a + c > b && c + b > a;
+}
