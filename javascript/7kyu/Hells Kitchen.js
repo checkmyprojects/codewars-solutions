71d3106a0f58103d9294ac7aa71f662e98028234
checkmyprojects <notnow.plz+github@gmail.com>
2024-08-13 06:44:38 +0200

    js 7kyu Hells Kitchen

---
+++ javascript/7kyu/Hells Kitchen.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/57d1f36705c186d018000813
+
+// Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+// Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+// Rules:
+// Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+
+function gordon(ramsay) {
+    return ramsay
+      .toUpperCase()
+      .replace(/\w+/g, '$&!!!!')
+      .replace(/[AEIOU]/g, v => v == 'A' ? '@' : '*');
+  }
