f9ebdbffd91b85dd14fa0968bb3946adc9eb2f2c
checkmyprojects <notnow.plz+github@gmail.com>
2024-08-18 13:12:19 +0200

    js 7kyu getNames()

---
+++ javascript/7kyu/getNames.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/514a677421607afc99000002
+
+// The following code is not giving the expected results. Can you debug what the issue is?
+
+// The following is an example of data that would be passed in to the function. 
+
+function getNames(data){
+    return data.map(function(item){return item.name});
+}
