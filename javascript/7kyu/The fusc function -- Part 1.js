cf97db6c316464b72925969c77bcc801c9a7408a
checkmyprojects <notnow.plz+github@gmail.com>
2024-10-20 09:24:00 +0200

    js 7kyu The fusc function -- Part 1

---
+++ javascript/7kyu/The fusc function -- Part 1.js
@@ -0,0 +1,35 @@
+// https://www.codewars.com/kata/570409d3d80ec699af001bf9
+
+// The fusc function is defined recursively as follows:
+
+// 1. fusc(0) = 0
+// 2. fusc(1) = 1
+// 3. fusc(2 * n) = fusc(n)
+// 4. fusc(2 * n + 1) = fusc(n) + fusc(n + 1)
+
+// The 4 rules above are sufficient to determine the value of fusc for any non-negative input n. For example, let's say you want to compute fusc(10).
+
+//     fusc(10) = fusc(5), by rule 3.
+//     fusc(5) = fusc(2) + fusc(3), by rule 4.
+//     fusc(2) = fusc(1), by rule 3.
+//     fusc(1) = 1, by rule 2.
+//     fusc(3) = fusc(1) + fusc(2) by rule 4.
+//     fusc(1) and fusc(2) have already been computed are both equal to 1.
+
+// Putting these results together fusc(10) = fusc(5) = fusc(2) + fusc(3) = 1 + 2 = 3
+
+// Your job is to produce the code for the fusc function. In this kata, your function will be tested with small values of n, so you should not need to be concerned about stack overflow or timeouts.
+
+// Hint: Use recursion.
+
+function fusc(n){
+    switch(true){
+      case n == 0:
+      case n == 1:
+        return n;
+      case n % 2 == 0:
+        return fusc(n / 2);
+      default:
+        return fusc((n - 1)/2) + fusc((n - 1)/2 + 1);
+    }
+  }
