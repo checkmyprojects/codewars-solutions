b46476692b92ebe432f33b403d3202ca33a2de4c
checkmyprojects <notnow.plz+github@gmail.com>
2024-10-26 10:01:37 +0200

    js 7kyu How Green Is My Valley?

---
+++ javascript/7kyu/How Green Is My Valley.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/56e3cd1d93c3d940e50006a4
+
+function makeValley(arr) {
+    var leftWing = []
+    var rightWing = []
+    arr.sort((a, b) => b - a)
+      .forEach((el, i) => i % 2 == 0 ? leftWing.push(el) : rightWing.unshift(el))
+    
+    return [...leftWing, ...rightWing]
+}
