c714966723e894d7a74f084ca54cf4391dc866e4
checkmyprojects <notnow.plz+github@gmail.com>
2024-11-02 10:39:35 +0100

    js 7kyu Find the index of the second occurrence of a letter in a string

---
+++ javascript/7kyu/Find the index of the second occurrence of a letter in a string.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/63f96036b15a210058300ca9
+
+// In this kata, you need to write a function that takes a string and a letter as input and then returns the index of the second occurrence of that letter in the string. If there is no such letter in the string, then the function should return -1. If the letter occurs only once in the string, then -1 should also be returned.
+
+// Examples:
+
+// second_symbol('Hello world!!!','l') --> 3
+// second_symbol('Hello world!!!', 'A') --> -1
+
+// Good luck ;) And don't forget to rate this Kata if you liked it.
+
+function secondSymbol(s, symbol) {
+    return s.indexOf(symbol, s.indexOf(symbol) + 1);
+}
