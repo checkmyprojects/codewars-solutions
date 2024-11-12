f39f511475f02bd2eb12810da9bedd835a92a3d0
checkmyprojects <notnow.plz+github@gmail.com>
2024-11-12 06:45:54 +0100

    js 7kyu LinkedList - Array

---
+++ javascript/7kyu/LinkedList - Array.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/557dd2a061f099504a000088
+
+// Linked lists are data structures composed of nested or chained objects, each containing a single value and a reference to the next object.
+
+// Here's an example of a list:
+// {value: 1, next: {value: 2, next: {value: 3, next: null}}}
+
+// Write a function listToArray (or list_to_array in Python) that converts a list to an array, like this:
+// [1, 2, 3]
+
+// Assume all inputs are valid lists with at least one value. For the purpose of simplicity, all values will be either numbers, strings, or Booleans.
+
+function listToArray(list) {
+    var array = [];
+    for (var node = list; node; node = node.next)
+      array.push(node.value);
+    return array;
+  }
