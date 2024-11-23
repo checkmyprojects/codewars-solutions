69e324a4b08821d060978ad7814f272068669349
checkmyprojects <notnow.plz+github@gmail.com>
2024-11-23 08:50:17 +0100

    js 7kyu Borrower Speak

---
+++ javascript/7kyu/Borrower Speak.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/57d2ba8095497e484e00002e
+
+// The borrowers are tiny tiny fictional people. As tiny tiny people they have to be sure they aren't spotted, or more importantly, stepped on.
+
+// As a result, the borrowers talk very very quietly. They find that capitals and punctuation of any sort lead them to raise their voices and put them in danger.
+
+// The young borrowers have begged their parents to stop using caps and punctuation.
+
+// Change the input text s to new borrower speak. Help save the next generation!
+
+function borrow(s){
+    return s.toLowerCase().replace(/\W/g, '');
+  }
