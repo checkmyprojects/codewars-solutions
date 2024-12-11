d7cacffd5a75d294dfb54f80f81b9a24ec73da80
checkmyprojects <notnow.plz+github@gmail.com>
2024-12-11 06:47:17 +0100

    js 7kyu Making Copies

---
+++ javascript/7kyu/Making Copies.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/53d2697b7152a5e13d000b82
+
+// Write a function that takes a list (in Python) or array (in other languages) of numbers, and makes a copy of it.
+
+// Note that you may have troubles if you do not return an actual copy, item by item, just a pointer or an alias for an existing list or array.
+
+// If not a list or array is given as a parameter in interpreted languages, the function should raise an error.
+
+function copyList(l) {
+    return l.slice();
+}
