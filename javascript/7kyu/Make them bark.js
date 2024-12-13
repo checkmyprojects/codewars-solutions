3cc9fcb506f8a51eca2ef1744bcf4e0b8c4e9797
checkmyprojects <notnow.plz+github@gmail.com>
2024-12-13 06:43:54 +0100

    js 7kyu Make them bark

---
+++ javascript/7kyu/Make them bark.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5535572c1de94ba2db0000f6
+
+// #Make them bark
+
+// You have been hired by a dogbreeder to write a program to keep record of his dogs.
+
+// You've already made a constructor Dog, so no one has to hardcode every puppy.
+
+Dog.prototype.bark = () => 'Woof!';
