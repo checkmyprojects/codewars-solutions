31c8c4690fc21b94459002271e3e59ce51e3c1a3
checkmyprojects <notnow.plz+github@gmail.com>
2025-01-02 13:34:38 +0100

    js 7kyu String ends with

---
+++ javascript/7kyu/String ends with.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
+
+function solution(str, ending){
+    return str.endsWith(ending);
+  }
