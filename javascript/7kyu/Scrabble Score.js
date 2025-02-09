81df83b062cb09eae44d828c556716fd1a8d2f44
checkmyprojects <notnow.plz+github@gmail.com>
2025-02-09 10:26:08 +0100

    js 7kyu Scrabble Score

---
+++ javascript/7kyu/Scrabble Score.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/558fa34727c2d274c10000ae
+
+// Write a program that, given a word, computes the scrabble score for that word.
+
+function scrabbleScore(word) {
+    array = word.toUpperCase().split('')
+    score = 0
+    
+    var dict = {
+      " ":0,
+      "A":1,"E":1,"I":1,"O":1,"U":1,"L":1,"N":1,"R":1,"S":1,"T": 1,
+      "D":2,"G":2,
+      "B":3,"C":3,"M":3,"P":3,
+      "F":4,"H":4,"V":4,"W":4,"Y":4,
+      "K":5, 
+      "J":8,"X":8,
+      "Q":10, "Z":10  
+    }
+  
+    array.forEach(function(key) {
+      return score += dict[key]
+    })
+  
+    return score
+}
