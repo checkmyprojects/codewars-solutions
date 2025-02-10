3078c81be3468f1b3da61df202682296cb32c1b8
checkmyprojects <notnow.plz+github@gmail.com>
2025-02-10 06:46:01 +0100

    js 7kyu Recursion 101

---
+++ javascript/7kyu/Recursion 101.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/5b752a42b11814b09c00005d
+
+// In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
+
+// i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+// ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+// iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+
+// a and b will both be lower than 10E8
+
+function solve(a,b){
+    if (a === 0 || b === 0){
+      return [a,b]
+    }
+
+    if(a >= 2*b){
+      a = a% (2*b)
+      return solve(a,b)
+    }
+  
+    if(b >= 2*a){
+      b = b%(2*a)
+      return solve(a,b)
+    }
+    
+    return[a,b]
+}
