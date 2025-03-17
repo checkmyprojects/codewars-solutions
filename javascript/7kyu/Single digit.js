05e2828bbe34e75f2b3421361080924fcec94a4f
checkmyprojects <notnow.plz+github@gmail.com>
2025-03-17 06:53:09 +0100

    js 7kyu Single digit

---
+++ javascript/7kyu/Single digit.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5a7778790136a132a00000c1
+
+// The goal of this Kata is to reduce the passed integer to a single digit (if not already) by converting the number to binary, taking the sum of the binary digits, and if that sum is not a single digit then repeat the process.
+
+//     If the passed integer is already a single digit there is no need to reduce
+
+//     n will be an integer such that 0 < n < 10⁹
+
+function singleDigit(n) {
+    while (n >= 10) {
+        n = (n.toString(2).match(/1/g) || []).length;
+    }
+    return n;
+}
