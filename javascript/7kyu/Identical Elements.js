c9cf2998ee1d6db723132144b6c1d2bfea1a2111
checkmyprojects <notnow.plz+github@gmail.com>
2025-06-03 06:44:35 +0200

    js 7kyu Identical Elements

---
+++ javascript/7kyu/Identical Elements.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/583ebb9328a0c034490001ba
+
+// Given two arrays of integers m and n, test if they contain at least one identical element. Return true if they do; false if not.
+
+// Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
+
+function duplicateElements(m, n) {
+    return m.some(v => n.includes(v))
+}
