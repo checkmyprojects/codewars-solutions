fed7744fa835ac6269a4aa036f4be3e4a13610bb
checkmyprojects <notnow.plz+github@gmail.com>
2025-08-11 06:47:43 +0200

    java 7kyu Cyclops numbers

---
+++ java/7kyu/Cyclops numbers.java
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/56b0bc0826814364a800005a
+
+// A cyclops number is a number in binary that is made up of all 1's, with one 0 in the exact middle. That means all cyclops numbers must have an odd number of digits for there to be an exact middle.
+// A couple examples:
+// 101
+// 11111111011111111
+// You must take an input, n, that will be in decimal format (base 10), then return True if that number wil be a cyclops number when converted to binary, or False if it won't.
+// Assume n will be a positive integer. 
+
+interface Kata {
+  static boolean cyclops(long n) {
+    return Long.toBinaryString(n).matches("(1+)0\\1");
+  }
+}
