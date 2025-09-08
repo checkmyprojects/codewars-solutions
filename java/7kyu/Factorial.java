5e087bbe30400e1da430789fb6dd081dd0806265
checkmyprojects <notnow.plz+github@gmail.com>
2025-09-08 06:41:26 +0200

    java 7kyu Factorial

---
+++ java/7kyu/Factorial.java
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc
+
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+// Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C)
+
+public class Factorial {
+  public int factorial(int n) {
+    if(n < 0 || n > 12) throw new IllegalArgumentException("Useless fuffery!");
+    return n <= 1 ? 1 : n * factorial(n - 1);
+  }
+}
