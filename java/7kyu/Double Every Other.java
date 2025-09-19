815f74b917d63f232f62cdaf4472be03583a7bc6
checkmyprojects <notnow.plz+github@gmail.com>
2025-09-19 06:44:36 +0200

    java 7kyu Double Every Other

---
+++ java/7kyu/Double Every Other.java
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5809c661f15835266900010a
+
+// Write a function that doubles every second integer in a list, starting from the left.
+// Example:
+// For input array/list :
+// [1,2,3,4]
+// the function should return :
+// [1,4,3,8]
+
+public class Kata {
+  public static int[] doubleEveryOther(int[] a) {
+    for (int i = 1; i < a.length; i += 2) a[i] *= 2;
+    return a;
+  }
+}
