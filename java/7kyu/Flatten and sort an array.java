fa053e631d18a3d75630fd7e1d2b49235065628d
checkmyprojects <notnow.plz+github@gmail.com>
2025-10-02 06:42:49 +0200

    java 7kyu Flatten and sort an array

---
+++ java/7kyu/Flatten and sort an array.java
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/57ee99a16c8df7b02d00045f
+
+// Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+// Example:
+// Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+import java.util.Arrays;
+public class Kata {
+	public static int[] flattenAndSort(int[][] array) {		
+		return Arrays.stream(array).flatMapToInt(Arrays::stream).sorted().toArray();
+	}
+}
