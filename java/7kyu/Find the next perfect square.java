9be8a842685321a547560e60235093336b80abe3
checkmyprojects <notnow.plz+github@gmail.com>
2025-10-06 06:41:46 +0200

    java 7kyu Find the next perfect square

---
+++ java/7kyu/Find the next perfect square.java
@@ -0,0 +1,16 @@
+// Friend or Foe?
+
+// https://www.codewars.com/kata/55b42574ff091733d900002f
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+public class NumberFun {
+  public static long findNextSquare(long sq) {
+      long root = (long) Math.sqrt(sq);
+      return root * root == sq ? (root + 1) * (root + 1) : -1;
+  }
+}
