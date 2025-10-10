a6f87f4a684955d4b64f2c349af5bdfc0efa85f0
checkmyprojects <notnow.plz+github@gmail.com>
2025-10-10 06:45:30 +0200

    java 7kyu Find the Squares

---
+++ java/7kyu/Find the Squares.java
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/60908bc1d5811f0025474291
+
+// Complete the function that takes an odd integer (0 < n < 1000000) which is the difference between two consecutive perfect squares, and return these squares as a string in the format "bigger-smaller"
+// Examples
+
+// 9  -->  "25-16"
+// 5  -->  "9-4"
+// 7  -->  "16-9"
+
+public class Solution {
+  public static String findSquares(final int n) {
+    final long a = (n + 1) / 2;
+    final long b = a - 1;
+    return String.format("%d-%d", a * a, b * b);
+  }
+}
