00eee141ff8c5191a0286eb1197c6087df5de390
checkmyprojects <notnow.plz+github@gmail.com>
2025-10-21 06:42:53 +0200

    java 7kyu Is this a triangle

---
+++ java/7kyu/Is this a triangle.java
@@ -0,0 +1,11 @@
+// Is this a triangle?
+
+// https://www.codewars.com/kata/56606694ec01347ce800001b
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+class TriangleTester{
+  public static boolean isTriangle(int a, int b, int c){
+    return a + b > c && a + c > b && c + b > a;
+  }
+}
