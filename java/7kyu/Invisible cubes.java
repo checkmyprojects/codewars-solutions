e162ae93126d7eb2ca259c07db47192d170ebc61
checkmyprojects <notnow.plz+github@gmail.com>
2025-10-25 09:01:03 +0200

    java 7kyu Invisible cubes

---
+++ java/7kyu/Invisible cubes.java
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/560d6ebe7a8c737c52000084
+
+// Imagine there's a big cube consisting of n3 n^3 n3 small cubes. Calculate, how many small cubes are not visible from outside.
+
+// For example, if we have a cube which has 4 cubes in a row, then the function should return 8, because there are 8 cubes inside our cube (2 cubes in each dimension)
+
+public class NotVisibleCubes {
+	public Long notVisibleCubes(Long n) {
+		Long a = n - 2;
+    if (a <= 0) {
+      return 0L;
+    }
+    return a * a * a;
+	}	
+}
