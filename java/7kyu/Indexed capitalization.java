9760f3b12dd7a83d55eed1cd24d8602f62ed01ee
checkmyprojects <notnow.plz+github@gmail.com>
2025-10-29 06:45:54 +0100

    java 7kyu Indexed capitalization

---
+++ java/7kyu/Indexed capitalization.java
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1
+
+// Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+// For example:
+
+//     capitalize("abcdef",[1,2,5]) = "aBCdeF"
+//     capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+
+// The input will be a lowercase string with no spaces and an array of digits.
+// Good luck!
+
+package kata;
+import java.util.*;
+public class Solution{
+    public static String capitalize(String s, int[] ind){
+        char[] chars = s.toCharArray();
+        Arrays.stream(ind)
+                .filter(c -> c < s.length())
+                .forEach(c -> chars[c] = Character.toUpperCase(chars[c]));
+        return new String(chars);
+    }
+}
