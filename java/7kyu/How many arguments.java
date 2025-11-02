80d59ebfd17d5e2f8f7c9017cff5510e72259944
checkmyprojects <notnow.plz+github@gmail.com>
2025-11-02 09:04:37 +0100

    java 7kyu How many arguments

---
+++ java/7kyu/How many arguments.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5c44b0b200ce187106452139
+
+// Write a function that returns the number of arguments it received.
+
+class Arguments {
+  public static int countArgs(Object... args) {
+    return args.length;
+	}
+}
