0536d7fc18b6adad49aba8b961b0a72ad3662724
checkmyprojects <notnow.plz+github@gmail.com>
2025-11-04 06:43:53 +0100

    java 7kyu Holiday VII - Local Talk

---
+++ java/7kyu/Holiday VII - Local Talk.java
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/57e92812750fcc051800004d
+
+// In Bali, as far as I can gather, when ex-pats speak to locals, they basically insert the word 'Pak' as often as possible. I am assured it means something like 'mate' or 'sir' but that could be completely wrong.
+
+// Anyway, as some basic language education(??) this kata requires you to turn any sentence provided (s) into ex-pat balinese waffle by inserting the word 'pak' between every other word. Simple 8kyu :D
+
+// Pak should not be the first or last word. Strings of just spaces should return an empty string.
+// Fundamentals
+// Strings
+// Arrays
+
+class Kata {
+    static String pak(final String s) {
+        return String.join(" pak ", s.split(" "));
+    }
+}
