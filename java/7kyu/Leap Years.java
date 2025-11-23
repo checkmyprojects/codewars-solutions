3865855c23438344404a472276c348421dfcf9b7
checkmyprojects <notnow.plz+github@gmail.com>
2025-11-23 09:09:39 +0100

    java 7kyu Leap Years

---
+++ java/7kyu/Leap Years.java
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/526c7363236867513f0005ca
+
+// In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+//     Years divisible by 4 are leap years,
+//     but years divisible by 100 are not leap years,
+//     but years divisible by 400 are leap years.
+
+public class LeapYears {
+
+    public static boolean isLeapYear(int year) {
+        return year % 100 == 0 ? year % 400 == 0 : year % 4 == 0;
+    }
+}
