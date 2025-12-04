02e1da2f564e9aa63762ec394fda3399a6eaa9ac
checkmyprojects <notnow.plz+github@gmail.com>
2025-12-04 06:47:53 +0100

    java 7kyu Make a function that does arithmetic

---
+++ java/7kyu/Make a function that does arithmetic.java
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/583f158ea20cfcbeb400000a
+
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply". 
+
+class ArithmeticFunction {
+  public static int arithmetic(int a, int b, String operator) {
+    switch(operator) {
+      case "add":
+        return a + b;
+      case "subtract":
+        return a - b;
+      case "multiply":
+        return a * b;
+      case "divide":
+        return a / b;
+    }
+    return 0;
+  }
+}
