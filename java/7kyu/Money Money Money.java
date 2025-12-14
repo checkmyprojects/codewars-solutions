ebf85f5d58284d049380ef2f4ead5a4ae543821c
checkmyprojects <notnow.plz+github@gmail.com>
2025-12-14 08:56:57 +0100

    java 7kyu Money Money Money

---
+++ java/7kyu/Money Money Money.java
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/563f037412e5ada593000114
+
+// Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+// Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+public class Money {
+  public static int calculateYears(double principal, double interest, double tax, double desired) {
+    int years = 0;
+    while (principal < desired) {
+      principal += principal * interest * (1 - tax);
+      years++;
+    }
+    return years;
+  }
+}
