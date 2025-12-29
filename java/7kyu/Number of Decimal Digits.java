2c75fb8435f5fe1324be73b7f8e23040e7d7d676
checkmyprojects <notnow.plz+github@gmail.com>
2025-12-29 08:58:42 +0100

    java 7kyu Number of Decimal Digits

---
+++ java/7kyu/Number of Decimal Digits.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/58fa273ca6d84c158e000052
+
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+public class DecTools {
+    public static int Digits(long n) {
+        return Long.toString(n).length();
+    }
+}
