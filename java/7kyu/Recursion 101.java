5afdbd7d61747258759ff5edf08ac084a5dee33f
checkmyprojects <notnow.plz+github@gmail.com>
2026-01-25 08:58:21 +0100

    java 7kyu Recursion 101

---
+++ java/7kyu/Recursion 101.java
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5b752a42b11814b09c00005d
+
+// In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
+
+// i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+// ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+// iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+
+// a and b will both be lower than 10E8
+
+class Solution{
+    public static int[] solve(int a, int b) {
+      return a == 0 || b == 0 ? new int[]{a, b} :
+             a >= 2 * b ? solve(a - 2 * b, b) :
+             b >= 2 * a ? solve(a, b - 2 * a) : new int[]{a, b};
+    }
+}
