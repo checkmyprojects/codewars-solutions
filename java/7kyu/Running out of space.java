874d7daf55980a58c21785922161986f6359619f
checkmyprojects <notnow.plz+github@gmail.com>
2026-02-12 06:47:31 +0100

    java 7kyu Running out of space

---
+++ java/7kyu/Running out of space.java
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/56576f82ab83ee8268000059
+
+// Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
+// For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+
+public class OutOfSpace {
+  
+  public static String[] spacey(String[] array) {
+    for(int i=1; i < array.length; i++) {
+        array[i] = array[i-1] + array[i]; 
+    }
+    return array;
+  }
+  
+}
