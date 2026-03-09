2e797a12a22ab7348a1e2cfa3c6365c3e3bb3ef9
checkmyprojects <notnow.plz+github@gmail.com>
2026-03-09 06:46:19 +0100

    java 7kyu Small enough - Beginner

---
+++ java/7kyu/Small enough - Beginner.java
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57cc981a58da9e302a000214
+
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+// You can assume all values in the array are numbers.
+
+public class Kata {
+    public static boolean smallEnough(int[] a, int limit) {
+        return java.util.Arrays.stream(a).noneMatch(i -> i>limit);
+    }
+}
