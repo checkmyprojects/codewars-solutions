641d43218bf775fefbc61ccc13ef7c81fcb5feed
checkmyprojects <notnow.plz+github@gmail.com>
2026-03-13 06:45:11 +0100

    java 7kyu Sort an array by value and index

---
+++ java/7kyu/Sort an array by value and index.java
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/58e0cb3634a3027180000040
+
+// Sort an array by value and index
+
+// Your task is to sort an array of integer numbers by the product of the value and the index of the positions.
+
+// For sorting the index starts at 1, NOT at 0!
+// The sorting has to be ascending.
+// The array will never be null and will always contain numbers.
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class Kata{
+	public static int[] sortByValueAndIndex(int[] array){
+		return IntStream.range(0, array.length).boxed().sorted(Comparator.comparingInt(i->array[i]*(i+1))).mapToInt(i->array[i]).toArray();
+	}
+}
