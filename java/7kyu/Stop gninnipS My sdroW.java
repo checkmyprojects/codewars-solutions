8e1392b20f7588f6dd8be18e9116781fca5ead61
checkmyprojects <notnow.plz+github@gmail.com>
2026-03-23 06:46:22 +0100

    java 7kyu Stop gninnipS My sdroW

---
+++ java/7kyu/Stop gninnipS My sdroW.java
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5264d2b162488dc400000001
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+import java.util.Arrays;
+
+public class SpinWords {
+
+  public String spinWords(String sentence) {
+    String[] words = sentence.split(" ");
+    for (int i=0; i<words.length; i++) {
+      if (words[i].length() >= 5) {
+        words[i] = new StringBuilder(words[i]).reverse().toString();
+      }
+    }
+    return String.join(" ",words);
+  }
+}
