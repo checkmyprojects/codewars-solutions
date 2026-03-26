fa46fda7d5e9d7c430a814d6330d2487e36588f5
checkmyprojects <notnow.plz+github@gmail.com>
2026-03-26 06:51:55 +0100

    java 7kyu String ends with

---
+++ java/7kyu/String ends with.java
@@ -0,0 +1,11 @@
+String ends with?
+
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
+
+public class Kata {
+  public static boolean solution(String str, String ending) {
+    return str.endsWith(ending);
+  }
+}
