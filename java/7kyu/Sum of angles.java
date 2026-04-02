392360553834814bb19588d5f8aeafade026a96a
checkmyprojects <notnow.plz+github@gmail.com>
2026-04-02 08:57:09 +0200

    java 7kyu Sum of angles

---
+++ java/7kyu/Sum of angles.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+public class AngleSum {
+  public static int sumOfAngles(int n) {
+    return (n - 2) * 180;
+  }
+}
