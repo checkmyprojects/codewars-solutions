36e750767a725dd5f2ceca72848f254b03186307
checkmyprojects <notnow.plz+github@gmail.com>
2026-04-08 06:46:45 +0200

    java 7kyu Switcheroo

---
+++ java/7kyu/Switcheroo.java
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57f759bb664021a30300007d
+
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+public class Switch {
+    public static String switcheroo(String x) {
+        return x.replace('a','_').replace('b','a').replace('_','b');
+    }
+}
