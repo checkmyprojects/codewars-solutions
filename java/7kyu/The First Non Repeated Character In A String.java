409b93a263ceff463d99c3ea0d89a93f7874fb54
checkmyprojects <notnow.plz+github@gmail.com>
2026-04-11 08:59:19 +0200

    java 7kyu The First Non Repeated Character In A String

---
+++ java/7kyu/The First Non Repeated Character In A String.java
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/570f6436b29c708a32000826
+
+// You need to write a function, that returns the first non-repeated character in the given string.
+
+// If all the characters are unique, return the first character of the string.
+// If there is no unique character, return null in JS or Java, None in Python, '\0' in C.
+
+// You can assume, that the input string has always non-zero length.
+
+public class FirstNonRepeated {
+    public static Character firstNonRepeated(String str) {
+        for(char ch :str.toCharArray()) if(str.indexOf(ch) == str.lastIndexOf(ch)) return ch;
+        return 0;
+    }
+}
