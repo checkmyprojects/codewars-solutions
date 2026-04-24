33905d8ed031a4485f89d0e8aa1053c87f2e6535
checkmyprojects <notnow.plz+github@gmail.com>
2026-04-24 06:44:33 +0200

    java 7kyu Triangle area

---
+++ java/7kyu/Triangle area.java
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/59bd84b8a0640e7c49002398
+
+// Task.
+
+// Calculate area of given triangle. Create a function t_area that will take a string which will represent triangle, find area of the triangle, one space will be equal to one length unit. The smallest triangle will have one length unit.
+
+// Hints
+
+// Ignore dots.
+
+// All triangles will be right isoceles.
+
+// Example:
+
+// .
+// .      .  
+// .      .       .      ---> should return 2.0
+
+// .
+// .      .  
+// .      .       .     
+// .      .       .      .      ---> should return 4.5
+
+public class Kata {
+    public static float tArea(String tStr) {
+        int h = tStr.split("\n").length - 2;
+        return 0.5f * h * h;
+    }
+}
