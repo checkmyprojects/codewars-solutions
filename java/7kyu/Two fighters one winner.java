9fa1f4c46396fe970035e87f71d669af2664dc37
checkmyprojects <notnow.plz+github@gmail.com>
2026-04-26 08:53:19 +0200

    java 7kyu Two fighters one winner

---
+++ java/7kyu/Two fighters one winner.java
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/577bd8d4ae2807c64b00045b
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+public class Kata {
+  public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+    Fighter a=fighter1, b=fighter2;
+    if (firstAttacker.equals(fighter2.name)) {
+      a = fighter2; b = fighter1;
+    }    
+    while (true) {      
+      if ((b.health -= a.damagePerAttack) <= 0) return a.name;  // a wins
+      if ((a.health -= b.damagePerAttack) <= 0) return b.name;  // b wins
+    }
+  }
+}
