7d8ac06d785737a21202d345d5dcff3cd7ece66f
checkmyprojects <notnow.plz+github@gmail.com>
2026-05-09 08:56:09 +0200

    java 7kyu shorter concat reverse longer

---
+++ java/7kyu/shorter concat reverse longer.java
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/54557d61126a00423b000a45
+
+// Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+// In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+// Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+// If a and b have the same length treat a as the longer producing b+reverse(a)+b
+
+public class ReverseLonger {
+    public static String shorterReverseLonger(String a, String b) {
+		String shorter = (a.length() < b.length()) ? a : b;
+        String longer = (a.length() < b.length()) ? b : a;
+		return shorter + reverse(longer) + shorter;
+	}
+
+    private static String reverse(String s) {
+    return new StringBuilder(s).reverse().toString();
+    }
+}
