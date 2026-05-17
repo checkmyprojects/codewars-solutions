fd4a492fb1bea405c6df8ad3de88c9b4d77229b4
checkmyprojects <notnow.plz+github@gmail.com>
2026-05-17 09:01:53 +0200

    java 7kyu Mumbling

---
+++ java/7kyu/Mumbling.java
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+// This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+public class Accumul {
+  public static String accum(String s) {
+    StringBuilder bldr = new StringBuilder();
+    int i = 0;
+    for(char c : s.toCharArray()) {
+      if(i > 0) bldr.append('-');
+      bldr.append(Character.toUpperCase(c));
+      for(int j = 0; j < i; j++) bldr.append(Character.toLowerCase(c));
+      i++;
+    }
+    return bldr.toString();
+  }
+}
