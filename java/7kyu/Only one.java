cd29297b0dc546d3d51edeb80f53c47447187bcd
checkmyprojects <notnow.plz+github@gmail.com>
2026-05-24 09:01:59 +0200

    java 7kyu Only one

---
+++ java/7kyu/Only one.java
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5734c38da41454b7f700106e
+
+// Task: Write a function which returns True if ONLY ONE of the boolean flag is True, otherwise return False. If there are no boolean flag, return False
+// Input 	Expected Output
+// No flags passed in 	False
+// True, False, False 	True
+// True, False, False, True 	False
+// False, False, False, False 	False
+
+import java.util.Arrays;
+
+public class OnlyOne {
+	public static boolean onlyOne(Boolean... args) {
+    return Arrays.stream(args).filter(Boolean.TRUE::equals).count() == 1;
+	}
+}
