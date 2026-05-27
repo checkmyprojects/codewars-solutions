6749fad15613bdc32cffcc79dd4770ad3151ee50
checkmyprojects <notnow.plz+github@gmail.com>
2026-05-27 06:46:26 +0200

    java 7kyu Sort by binary ones

---
+++ java/7kyu/Sort by binary ones.java
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/59eb28fb0a2bffafbb0000d6
+
+// In this example, you need to implement a function that sorts a list of integers based on their binary representation.
+
+// The rules are simple:
+
+//     sort the list based on the amount of 1s in the binary representation of each number.
+//     if two numbers have the same amount of 1s, the shorter string goes first. (ex: "11" goes before "101" when sorting 3 and 5 respectively)
+//     if the strings have the same length, lower decimal number goes first. (ex: 21 = "10101" and 25 = "11001", then 21 goes first as it's lower)
+
+// Examples:
+
+//     Input: [1,15,5,7,3]
+//         ( in binary strings is: ["1", "1111", "101", "111", "11"])
+
+//     Output: [15, 7, 3, 5, 1]
+//         (and after sortByBinaryOnes is: ["1111", "111", "11", "101", "1"])
+
+import java.util.Arrays;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.reverseOrder;
+
+class SortByBinaryOnes{
+    static Integer[] sort(final Integer[] list) {
+        return Arrays.stream(list)
+                .sorted(comparing(Integer::bitCount, reverseOrder())
+                        .thenComparing(Integer::highestOneBit)
+                        .thenComparing(Integer::compare))
+                .toArray(Integer[]::new);
+    }
+}
