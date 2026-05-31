7c2d14ee0e8dc3f05217bb85a84de94008f173a9
checkmyprojects <notnow.plz+github@gmail.com>
2026-05-31 08:52:07 +0200

    java 7kyu Two to One

---
+++ java/7kyu/Two to One.java
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Examples:
+
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+public class TwoToOne {
+    
+     public static String longest (String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+        (s1 + s2).chars().distinct().sorted().forEach(c -> sb.append((char) c));
+        return sb.toString();
+    }
+}
