9b023575d897866e8bb7885141018ab49c187cc4
checkmyprojects <notnow.plz+github@gmail.com>
2026-06-04 06:43:45 +0200

    java 7kyu Isograms

---
+++ java/7kyu/Isograms.java
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/54ba84be607a92aa900000f1
+
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class isogram {
+  public static boolean isIsogram(String str) {
+    Set<Character> letters = new HashSet<Character>();
+         
+    for (int i = 0; i < str.length(); ++i) {
+      if (letters.contains(str.toLowerCase().charAt(i))) {
+        return false;        
+      }
+      
+      letters.add(str.charAt(i));
+    }
+         
+    return true;
+  }
+}
