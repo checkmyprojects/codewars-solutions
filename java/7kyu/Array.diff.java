0767798cc672b38bbc5ed9e2e21e90fdca5c36fc
checkmyprojects <notnow.plz+github@gmail.com>
2026-06-08 06:42:05 +0200

    java 7kyu Array.diff

---
+++ java/7kyu/Array.diff.java
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/523f5d21c841566fde000009
+
+// Implement a function that computes the difference between two lists. The function should remove all occurrences of elements from the first list (a) that are present in the second list (b). The order of elements in the first list should be preserved in the result.
+// Examples
+
+// If a = [1, 2] and b = [1], the result should be [2].
+
+// If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Kata {
+    public static int[] arrayDiff(int[] a, int[] b) {
+        List<Integer> listA = Arrays.stream(a).boxed().collect(Collectors.toList());
+        List<Integer> listB = Arrays.stream(b).boxed().collect(Collectors.toList());
+        listA.removeAll(listB);
+        return listA.stream().mapToInt(e -> e).toArray();
+    }
+}
