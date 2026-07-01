2b1b11927f7d87025f8679176fe202acb9813d7b
checkmyprojects <notnow.plz+github@gmail.com>
2026-07-01 06:45:12 +0200

    java 7kyu Lottery machine

---
+++ javascript/7kyu/Lottery machine.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5832db03d5bafb7d96000107
+
+// Your task is to write an update for a lottery machine. Its current version produces a sequence of random letters and integers (passed as a string to the function). Your code must filter out all letters and return unique integers as a string, in their order of first appearance. If there are no integers in the string return "One more run!"
+// Examples
+
+// "hPrBKWDH8yc6Lt5NQZWQ"  -->  "865"
+// "ynMAisVpHEqpqHBqTrwH"  -->  "One more run!"
+// "555"                   -->  "5"
+
+
+function lottery(str){
+    res = str.replace(/[a-zA-Z]/g,'')
+      .split('')
+      .filter((x,n,s) => s.indexOf(x) == n)
+      .join('');
+    return res ? res : 'One more run!'
+ }
