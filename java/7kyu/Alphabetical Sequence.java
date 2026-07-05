4da94b8722095393c80408feb52a96b25f138acb
checkmyprojects <notnow.plz+github@gmail.com>
2026-07-05 09:03:50 +0200

    java 7kyu Alphabetical Sequence

---
+++ java/7kyu/Alphabetical Sequence.java
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/5bd00c99dbc73908bb00057a
+
+// In this kata you will be given a random string of letters and tasked with returning them as a string of comma-separated sequences sorted alphabetically, with each sequence starting with an uppercase character followed by n-1 lowercase characters, where n is the letter's alphabet position 1-26.
+// Example
+
+// "ZpglnRxqenU" -> "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"
+
+// Technical Details
+
+//     The string will include only letters.
+//     The first letter of each sequence is uppercase followed by n-1 lowercase.
+//     Each sequence is separated with a comma.
+//     Return value needs to be a string.
+
+import static java.util.stream.Collectors.joining;
+
+public class Solution {
+  public static String alphaSeq(String s) {
+      return s.chars()
+              .mapToObj(letter -> String.valueOf((char)letter))
+              .map(String::toLowerCase)
+              .sorted()
+              .map(letter -> letter.toUpperCase() + letter.repeat(letter.charAt(0) - 'a'))
+              .collect(joining(","));
+  }
+}
