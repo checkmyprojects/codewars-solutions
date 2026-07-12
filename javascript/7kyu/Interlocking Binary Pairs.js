7d66045bea0e8bd831e02656c5bfd341a3052801
checkmyprojects <notnow.plz+github@gmail.com>
2026-07-12 08:58:00 +0200

    js 7kyu Interlocking Binary Pairs

---
+++ javascript/7kyu/Interlocking Binary Pairs.js
@@ -0,0 +1,41 @@
+// https://www.codewars.com/kata/628e3ee2e1daf90030239e8a
+
+// Task
+
+// Write a function that checks if two non-negative integers make an "interlocking binary pair".
+// Interlock ?
+
+//     numbers can be interlocked if their binary representations have no 1's in the same place
+//     comparisons are made by bit position, starting from right to left (see the examples below)
+//     when representations are of different lengths, the unmatched left-most bits are ignored
+
+// Examples
+
+//     a = 9, b = 4
+
+//     Stacking representations shows how they can interlock.
+
+//      9    1001
+//      4     100
+
+//     Here, no 1's share any position, so the function returns true.
+
+//     a = 3, b = 6
+
+//     These representations do not interlock.
+
+//      3      11
+//      6     110
+
+//     Finding they both have a 1 in the same position, the function returns false.
+
+// Input
+
+// Two non-negative integers.
+// Output
+
+// Boolean true or false whether or not these integers are interlockable.
+
+function interlockable(a, b) {
+    return !(a & b);
+}
