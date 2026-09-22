11f8b9c0013becef06b2c81a24245f568d27bcb3
checkmyprojects <notnow.plz+github@gmail.com>
2026-09-22 06:45:31 +0200

    js 7kyu Apparently-Modifying Strings

---
+++ javascript/7kyu/Apparently-Modifying Strings.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/5b049d57de4c7f6a6c0001d7
+
+// For every string, after every occurrence of 'and' and/or 'but', insert the substring 'apparently' directly after the occurrence(s).
+
+// If input does not contain 'and' or 'but', return the same string. If a blank string, return ''.
+
+// If substring 'apparently' is already directly after an 'and' and/or 'but', do not add another. (Do not add duplicates).
+// Examples:
+
+// Input 1
+
+// 'It was great and I've never been on live television before but sometimes I don't watch this.'
+
+// Output 1
+
+// 'It was great and apparently I've never been on live television before but apparently sometimes I don't watch this.'
+
+// Input 2
+
+// 'but apparently'
+
+// Output 2
+
+// 'but apparently' 
+
+// (no changes because 'apparently' is already directly after 'but' and there should not be a duplicate.)
+
+// An occurrence of 'and' and/or 'but' only counts when it is at least one space separated. For example 'andd' and 'bbut' do not count as occurrences, whereas 'b but' and 'and d' does count.
+
+function apparently(string) {
+    return string.split(' ').reduce((o,c,i,a)=>{
+    o.push(c);
+    if(c === 'and' || c === 'but')
+        {
+        if(a[i+1] !== 'apparently')
+        { o.push('apparently') }
+        }
+        return o;
+    },[]).join(' ');
+}
