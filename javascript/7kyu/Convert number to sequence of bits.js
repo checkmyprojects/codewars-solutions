706b7f116f88ab9c0ea61993246960cd05b570c0
checkmyprojects <notnow.plz+github@gmail.com>
2026-09-24 06:39:26 +0200

    js 7kyu Convert number to sequence of bits

---
+++ javascript/7kyu/Convert number to sequence of bits.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/56536e45bd4717003a000056
+
+// Define the toBits(minLength) function converting provided number to a sequence of bits. The default minimum length should be 8.
+// Examples
+
+// (0).toBits() === '00000000';
+// (7).toBits() === '00000111';
+// (128).toBits() === '10000000';
+// (255).toBits() === '11111111';
+
+// (0).toBits(0) === '0';
+// (7).toBits(2) === '111';
+// (128).toBits(4) === '10000000';
+// (255).toBits(4) === '11111111';
+
+Number.prototype.toBits = function(len = 8) {
+    return this.toString`2`.padStart(len, '0')
+}
